7c365d48daa7e3e271741aaedf53388d9e176c69
Nick Winnubst <nwinnubst@gmail.com>
2017-07-19 00:33:27 +0200

    Added more tests Corrected bug using faulty "or" syntax

--- Risk/Tests/Controller/BattleTest.py
+++ Risk/Tests/Controller/BattleTest.py
@@ -6,8 +6,18 @@ import random
 class TestBattle(unittest.TestCase):
 
     def setUp(self):
-        self.amount = 3
-        self.possible_dice_rolls = [1,2,3,4,5,6]
+        self.valid_dice_rolls = [1, 2, 3, 4, 5, 6]
+
+        self.valid_amount_of_attacking_dice = [1,2,3]
+        self.valid_amount_of_defending_dice = [1,2]
+
+        self.valid_attack_scores = [-2,-1,0,1,2]
+
+        self.faulty_attacking_dice_1 = 4
+        self.faulty_attacking_dice_2 = 0
+
+        self.faulty_defending_dice_1 = 3
+        self.faulty_defending_dice_2 = 0
 
         self.attacking_die_1 = 4
         self.defending_die_1 = 3
@@ -18,6 +28,21 @@ class TestBattle(unittest.TestCase):
         self.attacking_die_3 = 4
         self.defending_die_3 = 6
 
+        self.amount = 3
+
+    def test_attack(self):
+
+        self.assertEqual(attack(self.faulty_attacking_dice_1, self.faulty_defending_dice_1), 0)
+        self.assertEqual(attack(self.faulty_attacking_dice_1, self.faulty_defending_dice_2), 0)
+        self.assertEqual(attack(self.faulty_attacking_dice_2, self.faulty_defending_dice_1), 0)
+        self.assertEqual(attack(self.faulty_attacking_dice_2, self.faulty_defending_dice_2), 0)
+
+        self.assertEqual(attack(self.faulty_attacking_dice_1, random.choice(self.valid_amount_of_defending_dice)), 0)
+        self.assertEqual(attack(self.faulty_attacking_dice_2, random.choice(self.valid_amount_of_defending_dice)), 0)
+        self.assertEqual(attack(random.choice(self.valid_amount_of_attacking_dice), self.faulty_defending_dice_1), 0)
+        self.assertEqual(attack(random.choice(self.valid_amount_of_attacking_dice), self.faulty_defending_dice_2), 0)
+
+        self.assertIn(attack(random.choice(self.valid_amount_of_attacking_dice), random.choice(self.valid_amount_of_defending_dice)), self.valid_attack_scores)
 
     def test_compare_dice(self):
         self.assertEqual(compare_dice(self.attacking_die_1,self.defending_die_1), 1)
@@ -31,7 +56,7 @@ class TestBattle(unittest.TestCase):
         self.assertGreaterEqual(dice[0],dice[1])
         self.assertGreaterEqual(dice[1],dice[2])
 
-        self.assertIn(dice[0], self.possible_dice_rolls)
-        self.assertIn(dice[1], self.possible_dice_rolls)
-        self.assertIn(dice[2], self.possible_dice_rolls)
+        self.assertIn(dice[0], self.valid_dice_rolls)
+        self.assertIn(dice[1], self.valid_dice_rolls)
+        self.assertIn(dice[2], self.valid_dice_rolls)
 
