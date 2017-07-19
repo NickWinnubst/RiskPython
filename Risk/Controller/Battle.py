6103f19818d8766ef5262a9dde047f3d67827978
Nick Winnubst <nwinnubst@gmail.com>
2017-07-19 15:51:33 +0200

    Comments for the battle file

--- Risk/Controller/Battle.py
+++ Risk/Controller/Battle.py
@@ -1,6 +1,7 @@
 import random
 
 
+# simulate a single attack with attackers and defenders
 def attack(attackers, defenders):
     if attackers <= 0 or defenders <= 0 or attackers > 3 or defenders > 2:
         return 0
@@ -16,12 +17,14 @@ def attack(attackers, defenders):
     return score
 
 
+# compare dice to determine a score
 def compare_dice(attacker_die, defender_die):
     if attacker_die > defender_die:
         return 1
     return -1
 
 
+# throw a given amount of dice
 def throw_dice(amount):
     dice = []
     for i in range(1,amount+1):
