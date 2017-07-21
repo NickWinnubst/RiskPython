04bce4108c46e600bbd813a9a49c3f7a6bf4a9d2
Nick Winnubst <nwinnubst@gmail.com>
2017-07-21 14:30:35 +0200

    Added reinfocements and proper turn ending Added selected countries highlighting

--- Risk/View/Board.py
+++ Risk/View/Board.py
@@ -3,6 +3,8 @@ from Model import MapLoader
 from Controller import SetUpGame
 from Controller.Battle import attack
 from Model.Territory import *
+from Controller.Reinforce import get_reinforcements
+
 
 class Board(tk.Frame):
 
@@ -13,13 +15,14 @@ class Board(tk.Frame):
 
         self.players = [["Fred","blue"],["George","green"],["Ron","red"]]
 
-        self.current_player_nr = 0
-
         self.game_map = MapLoader.load_fresh_map("../Maps/risk-1-original.json")
         self.game_map = SetUpGame.set_up_game([p[0] for p in self.players], self.game_map)
 
+        self.current_player_nr = 0
+        self.reinforcements = get_reinforcements(self.players[self.current_player_nr%len(self.players)][0],self.game_map)
+
         self.previous = Territory("None")
-        self.attack = [self.previous,self.previous]
+        self.fighting_countries = [self.previous, self.previous]
 
         self.display = tk.PhotoImage(file="../Maps/risk-1-original.png")
         self.display_map = tk.Label(root, image=self.display)
@@ -30,63 +33,81 @@ class Board(tk.Frame):
         self.game_map.print()
 
     def next_turn(self):
+        if self.reinforcements > 0:
+            return
         self.current_player_nr += 1
+        self.reinforcements = get_reinforcements(self.players[self.current_player_nr%len(self.players)][0],self.game_map)
         self.update()
 
     def left_click(self,event):
-        self.attack = self.print_location_data(event)
+        self.fighting_countries = self.location_data(event)
         try:
             self.pop_up.destroy()
         except AttributeError:
             ()
-        if self.attack[0].name in self.attack[1].connections and self.attack[0].armies > 1 and self.attack[0].owner is not self.attack[1].owner and self.attack[0].owner is self.players[self.current_player_nr%len(self.players)][0]:
+        if self.reinforcements > 0 and self.fighting_countries[1].owner is self.players[self.current_player_nr%len(self.players)][0]:
+            self.fighting_countries[1].armies += 1
+            self.reinforcements -= 1
+        elif self.fighting_countries[0].name in self.fighting_countries[1].connections and self.fighting_countries[0].armies > 1 and self.fighting_countries[0].owner is not self.fighting_countries[1].owner and self.fighting_countries[0].owner is self.players[self.current_player_nr%len(self.players)][0]:
             self.pop_up_attack()
 
-    def print_location_data(self,event):
+        self.update()
+
+    def location_data(self,event):
         selected = self.game_map.get_closest_territory([event.x,event.y])
+        self.print_location_data(selected,event)
+
+        previous = self.previous
+        self.previous = selected
+        return previous, selected
+
+    def print_location_data(self,selected, event):
         print("----------------------------------")
         print("Clicked at: %s, %s" % (event.x,event.y))
         print("Closest territory: %s" % selected.name)
         print("Territory owner: %s" % selected.owner)
         print("Army count: %s" % selected.armies)
         print("Connected to %s: %s" % (self.previous.name, self.previous.name in selected.connections))
-        previous = self.previous
-        self.previous = selected
-        return (previous, selected)
 
+    # Query attack
     def pop_up_attack(self):
         self.pop_up = tk.Toplevel()
         self.pop_up.geometry("20x40+590+400")
-        label1 = tk.Label(self.pop_up, text="Attack?")
+        label1 = tk.Label(self.pop_up, text="Attack " + self.fighting_countries[1].name + "?")
         label1.pack()
         label2 = tk.Button(self.pop_up, text="GO!", command=self.execute_attack)
         label2.pack()
 
     def execute_attack(self):
         success = False
-        if self.attack[0].armies > 1 and self.attack[1].armies > 0 and self.attack[0].owner is not self.attack[1].owner:
-            attackers_lost, defenders_lost = attack(min(3,self.attack[0].armies-1),min(2,self.attack[1].armies))
-            self.attack[0].armies -= attackers_lost
-            self.attack[1].armies -= defenders_lost
-        if self.attack[1].armies == 0:
-            self.attack[1].armies = self.attack[0].armies-1
-            self.attack[0].armies = 1
-            self.attack[1].owner = self.attack[0].owner
+        if self.fighting_countries[0].armies > 1 and self.fighting_countries[1].armies > 0 and self.fighting_countries[0].owner is not self.fighting_countries[1].owner:
+            attackers_lost, defenders_lost = attack(min(3, self.fighting_countries[0].armies - 1), min(2, self.fighting_countries[1].armies))
+            self.fighting_countries[0].armies -= attackers_lost
+            self.fighting_countries[1].armies -= defenders_lost
+        if self.fighting_countries[1].armies == 0:
+            self.fighting_countries[1].armies = self.fighting_countries[0].armies - 1
+            self.fighting_countries[0].armies = 1
+            self.fighting_countries[1].owner = self.fighting_countries[0].owner
             success = True
 
-        if self.attack[0].armies == 1:
+        if self.fighting_countries[0].armies == 1:
             self.pop_up.destroy()
             if success is not True:
                 self.previous = Territory("None")
         self.update()
 
+    # Update the visuals.
     def update(self):
         for label in self.display_map.winfo_children():
             label.destroy()
         for territory in self.game_map.get_all_territories():
             label = tk.Label(self.display_map, text=str(territory.armies))
             label.place(x=territory.location[0], y=territory.location[1])
-            label.configure(font="helvetica 14 bold",relief="raised", foreground="white", background=self.players[[p[0] for p in self.players].index(territory.owner)][1])
+            label.configure(font="helvetica 14",relief="raised", foreground="white", background=self.players[[p[0] for p in self.players].index(territory.owner)][1])
+            if self.fighting_countries[1] is territory:
+                label.configure(font="helvetica 20 bold")
+            elif self.fighting_countries[0] is territory and self.fighting_countries[0].name in self.fighting_countries[1].connections:
+                label.configure(font="helvetica 20 bold")
 
         self.current_player_label = tk.Label(self.display_map, text=self.players[self.current_player_nr%len(self.players)][0])
         self.current_player_label.configure(font="helvetica 14 bold",relief="raised", foreground="white", background=self.players[self.current_player_nr%len(self.players)][1])
@@ -95,6 +116,13 @@ class Board(tk.Frame):
         next_turn_button = tk.Button(self.display_map, text="Next Turn", command=self.next_turn)
         next_turn_button.configure(font="helvetica 14 bold",relief="raised", foreground="white", background="black")
         next_turn_button.place(x=1000,y=400)
+
+        if self.reinforcements > 0:
+            self.reinforcements_label = tk.Button(self.display_map, text="Reinforce: " + str(self.reinforcements), command=self.next_turn)
+        else:
+            self.reinforcements_label = tk.Button(self.display_map, text="Battle!", command=self.next_turn)
+        self.reinforcements_label.configure(font="helvetica 14 bold",relief="raised", foreground="white", background=self.players[self.current_player_nr%len(self.players)][1])
+        self.reinforcements_label.place(x=1000,y=300)
 
         self.display_map.pack()
 
